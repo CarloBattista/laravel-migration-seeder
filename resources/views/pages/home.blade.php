d4413acb8d54464a249581aca1d03ffb68f38af3
Cicidong00 <136182931+Cicidong00@users.noreply.github.com>
2023-06-12 17:10:24 +0200

    mi sono dimenticato di pushare

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,7 +9,19 @@
 <div class="mainView">
     <div class="container mb-5 mt-5 grid">
         <div class="row gap-3">
-            <div class="card"></div>
+            @foreach ($trains as $elem )
+                <div class="col">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $elem['azienda'] }}</h5>
+                            <p class="card-text">In orario: {{ $elem->in_orario ? 'Sì' : 'No' }}</p>
+                            <p class="card-text">Stato volo: {{ $elem->cancellato ? 'Cancellato' : 'In viaggio' }}</p>
+                            <p class="card-text">{{ $elem['orario_arrivo'] }}</p>
+                            <p class="card-text">Codice treno: <span class="text-warning">{{ $elem['codice_treno'] }}</span></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
